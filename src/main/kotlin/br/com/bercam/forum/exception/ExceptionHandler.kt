54660423d2791e552998889b70c09f8d9e512593
rafaelbercam <rbercam.medeiros@gmail.com>
2022-08-16 13:24:38 -0300

    Create handlers exception and bad request

--- src/main/kotlin/br/com/bercam/forum/exception/ExceptionHandler.kt
+++ src/main/kotlin/br/com/bercam/forum/exception/ExceptionHandler.kt
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestControllerAdvice
+import org.springframework.web.client.HttpClientErrorException.BadRequest
 import javax.servlet.http.HttpServletRequest
 
 @RestControllerAdvice
@@ -18,6 +19,34 @@ class ExceptionHandler {
         return ErrorView(
             status = HttpStatus.NOT_FOUND.value(),
             error = HttpStatus.NOT_FOUND.name,
+            message = exception.message,
+            path = request.servletPath
+        )
+    }
+
+    @ExceptionHandler(Exception::class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    fun handlerServerError(
+        exception: Exception,
+        request: HttpServletRequest
+    ): ErrorView {
+        return ErrorView(
+            status = HttpStatus.INTERNAL_SERVER_ERROR.value(),
+            error = HttpStatus.INTERNAL_SERVER_ERROR.name,
+            message = exception.message,
+            path = request.servletPath
+        )
+    }
+
+    @ExceptionHandler(BadRequest::class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    fun handlerBadRequest(
+        exception: Exception,
+        request: HttpServletRequest
+    ): ErrorView {
+        return ErrorView(
+            status = HttpStatus.BAD_REQUEST.value(),
+            error = HttpStatus.BAD_REQUEST.name,
             message = exception.message,
             path = request.servletPath
         )
