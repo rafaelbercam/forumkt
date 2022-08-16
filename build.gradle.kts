75eb2cef2b43089b8a19439edae6a601ec2e2b0c
rafaelbercam <rbercam.medeiros@gmail.com>
2022-08-16 14:00:31 -0300

    Create config h2 db and jpa dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,12 +16,14 @@ repositories {
 }
 
 dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa:2.7.2")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
+	testImplementation("com.h2database:h2:2.1.214")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
